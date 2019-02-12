69de4f360611156af3a83c66eccf8bb6f23cf2f3
arthur0420 <arthur0420@aliyun.com>
2019-02-12 23:03:53 +0800

    地址码

--- Project/settings/Uart3.Debug.cspy.ps1
+++ Project/settings/Uart3.Debug.cspy.ps1
@@ -23,9 +23,9 @@ param([String]$debugfile = "");
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\holdman\22-Uart3\Project\settings\Uart3.Debug.general.xcl" --backend -f "D:\holdman\22-Uart3\Project\settings\Uart3.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\holdman\mcu\Project\settings\Uart3.Debug.general.xcl" --backend -f "D:\holdman\mcu\Project\settings\Uart3.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\holdman\22-Uart3\Project\settings\Uart3.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\holdman\22-Uart3\Project\settings\Uart3.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\holdman\mcu\Project\settings\Uart3.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\holdman\mcu\Project\settings\Uart3.Debug.driver.xcl" 
 }
